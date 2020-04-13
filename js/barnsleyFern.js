4623c8b759eda2f316057e6b1303887effebce53
Haolin Yang <haolinyang95@gmail.com>
2020-04-13 17:10:41 -0400

    Updates

--- js/barnsleyFern.js
+++ js/barnsleyFern.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('canvas2');
+const canvas = document.getElementById('canvas-barnsley-fern');
 const ctx = canvas.getContext('2d');
 
 const WIDTH = canvas.width;
